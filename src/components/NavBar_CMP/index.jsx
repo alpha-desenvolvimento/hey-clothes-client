5177ebdcb3b79bb8302c0ac584657713a83bf35b
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-03 18:40:02 -0300

    updated NavBar

--- src/components/NavBar_CMP/index.jsx
+++ src/components/NavBar_CMP/index.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
 
-import { FiCoffee } from "react-icons/fi";
-import { NavBar as Nav, NavItemsWrapper, NavItem} from './styles';
+import { FiCoffee, FiHome } from "react-icons/fi";
+import { NavBar as Nav, NavItemsWrapper, NavItem, NavLink, LinkText} from './styles';
 
 const NavBar = () => {
     return(
     <Nav>
         <NavItemsWrapper>
             <NavItem>
-                <FiCoffee />
+                <NavLink>
+                    <FiHome />
+                    <LinkText>INICIO</LinkText>
+                </NavLink>
             </NavItem>
             <NavItem>
-                <FiCoffee />
+                <NavLink>
+                    <FiCoffee />
+                    <LinkText>LALALALA</LinkText>
+                </NavLink>
             </NavItem>
             <NavItem>
-                <FiCoffee />
+                <NavLink>
+                    <FiCoffee />
+                    <LinkText>LALALALA</LinkText>
+                </NavLink>
             </NavItem>
             <NavItem>
-                <FiCoffee />
+                <NavLink>
+                    <FiCoffee />
+                    <LinkText>LALALALA</LinkText>
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink>
+                    <FiCoffee />
+                    <LinkText>LALALALA</LinkText>
+                </NavLink>
             </NavItem>
         </NavItemsWrapper>
     </Nav>
--- src/components/NavBar_CMP/styles.jsx
+++ src/components/NavBar_CMP/styles.jsx
@@ -1,10 +1,36 @@
 import styled from 'styled-components';
+import { mediaQuery } from '../../styles/global'
+
+export const LinkText = styled.span`
+    font-size: 1.6rem;
+    display: none;
+    margin-left: 1.6rem;
+`
 
 export const NavBar = styled.nav`
-    width: 8rem;
-    height: 100vh;
     position: fixed;
-    background-color: "#B8B8B8";
+    bottom: 0;
+    width: 100vw;
+    height: 8rem;
+
+    background-color: #B8B8B8;
+
+    transition: width 200ms ease;
+    overflow: hidden;
+
+    
+    ${mediaQuery[0]}{
+        width: 8rem;
+        height: 100vh;
+
+        &:hover {
+            width: 24rem;
+        }
+
+        &:hover ${LinkText}{
+            display: block;
+        }
+    }
 `
 
 export const NavItemsWrapper = styled.ul`
@@ -12,13 +38,45 @@ export const NavItemsWrapper = styled.ul`
     padding: 0;
     margin: 0;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    align-items: center;
+    height: 100%;
+
+    ${mediaQuery[0]}{
+        flex-direction: column;
+    }
 `
 
 export const NavItem = styled.li`
     width: 100%;
-
-    &::last-child {
+    
+    &:last-child {
         margin-top: auto;
     }
+`
+
+export const NavLink = styled.a`
+  cursor: pointer;
+  display: inline-flex;
+  position: relative;
+  align-items: center;
+  justify-content: space-between;
+  vertical-align: middle;
+  height: 8rem;
+  color: #3B3B3B;
+  font-size: 2rem;
+  text-decoration: none;
+  transition: 200ms ease-in-out;
+
+
+  &:hover {
+    filter: grayscale(0%) opacity(1);
+    color: #141418;
+  }
+
+  & > svg {
+    font-size: 4rem;
+    min-width: 4rem;
+    margin: 0 0 0 2rem;
+  }
 `
