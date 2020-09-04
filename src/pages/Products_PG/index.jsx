022fa751624b02bae2cb4f87f1b85a27455ae63a
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-04 01:02:23 -0300

    updated Products_PG, implemented Cards & CardContainer

--- src/pages/Products_PG/index.jsx
+++ src/pages/Products_PG/index.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
 import NavBar, { Main } from '../../components/NavBar_CMP'
+import CardContainer from '../../components/CardContainer_CMP'
+import Card from '../../components/ProductCard_CMP'
 
 const Products_PG = () => {
     return(
         <>
             <NavBar />
             <Main>
-                <h1>hej</h1>
+                <div style={{margin: "4rem auto"}} >
+                    <h1 style={{width:"100%", textAlign: "center"}}>PLACEHOLDER DA BARRA DE PESQUISA</h1>
+                </div>
+                <CardContainer>
+                    <Card name={"Camisa cinza"} price={"5,00"} photo={"https://images.unsplash.com/photo-1564584217132-2271feaeb3c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"} />
+                    
+                    <Card name={"Camisa cinza"} price={"5,00"} photo={"https://images.unsplash.com/photo-1564584217132-2271feaeb3c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"} />
+
+                    <Card name={"Camisa cinza"} price={"5,00"} photo={"https://images.unsplash.com/photo-1564584217132-2271feaeb3c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"} />
+
+                    <Card name={"Camisa cinza"} price={"5,00"} photo={"https://images.unsplash.com/photo-1564584217132-2271feaeb3c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"} />
+                    
+                    <Card name={"Camisa cinza"} price={"5,00"} photo={"https://images.unsplash.com/photo-1564584217132-2271feaeb3c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"} />
+                </CardContainer>
             </Main>
         </>
     )
