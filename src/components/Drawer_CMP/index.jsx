02763dc9107db4c1e1fb8f501003a16d938cc228
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-04 11:50:19 -0300

    moved useDrawerUtils to Drawer_CMP

--- src/components/Drawer_CMP/index.jsx
+++ src/components/Drawer_CMP/index.jsx
@@ -19,9 +19,23 @@ const Drawer = ({ isOpen, hide, children }) => {
 
 export default Drawer;
 
+export const useDrawerUtils = () => {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const hideDrawer = () => {
+        setIsOpen(false)
+    }
+
+    const openDrawer = () =>{
+        setIsOpen(true);
+    }
+
+    return [isOpen, hideDrawer, openDrawer];
+}
 
 /*
 @author Igor Bedesqui
 @descrition Componente usado para criar uma "gaveta", é usado o react Portal para cria-la fora do root, tornando possivel que o componente seja renderizado acima da aplicação inteira.
 O componente deve receber um prop isOpen que define se será aberto ou não e um prop hide que deve ser um método com o poder de alterar isOpen
+Também exporta um Hook que supre as necessidades do componente Drawer, esse hook retorna uma aray com o estado isOpen para passar ao Drawer, o método hideDrawer para esconder o drawer(também deve ser passado ao Drawer) e um método openDrawer para abrir o drawer
 */
