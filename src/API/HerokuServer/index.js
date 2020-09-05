a6820944b6b3a8609da9f35cbf16263256b583e5
Gabriel Loch <grl.loch@gmail.com>
2020-09-05 02:19:34 -0300

    Updated Heroku Server API

--- src/API/HerokuServer/index.js
+++ src/API/HerokuServer/index.js
@@ -1,5 +1,7 @@
 import Auth from "./auth";
+import Product from './product'
 
 export default class HerokuServer {
   static Auth = Auth;
+  static Product = Product;
 }
---
+++ src/API/HerokuServer/mock.js
@@ -0,0 +1,6 @@
+export const product = {
+  id: 1,
+  name: "Camiseta Cinza Masculina Renner",
+  price: 50,
+  pictures: [],
+};
