66480cc83743df63b5e9b380babdbd08ca620ef0
Gabriel Loch <grl.loch@gmail.com>
2020-09-05 02:20:09 -0300

    Update typography.jsx

--- src/styles/typography.jsx
+++ src/styles/typography.jsx
@@ -2,11 +2,11 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
     html{font-size:10px;}
-    h1{font-size: 5.96rem}
-    h2{font-size: 4.768rem}
-    h3{font-size: 3.815rem}
+    h1,.h1-font-size{font-size: 5.96rem}
+    h2,.h2-font-size{font-size: 4.768rem}
+    h3,.h3-font-size{font-size: 3.815rem}
     h4, h4-font-size{font-size: 3.052rem}
-    h5{font-size: 2.441rem}
-    h6{font-size: 1.953rem}
-    p {font-size: 1.563rem}
+    h5,.h5-font-size{font-size: 2.441rem}
+    h6,.h6-font-size{font-size: 1.953rem}
+    p,.p-font-size {font-size: 1.563rem}
 `;
