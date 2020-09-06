84b103657f35db00b70e4009a044e63202f6172f
Gabriel Loch <grl.loch@gmail.com>
2020-09-06 20:31:32 -0300

    Heroku Server API Updated

--- src/API/HerokuServer/user.js
+++ src/API/HerokuServer/user.js
@@ -14,8 +14,11 @@ export default class User {
   static async get(args = { id: null }) {
     const { id } = args;
 
-    for (const user of users) if (user.id == id) return user;
-    
+    console.log('id: ', id);
+    for (const user of users) {
+      console.log('user: ',user);
+      if (user.id == id) return user;
+    }
     return null;
   }
 
