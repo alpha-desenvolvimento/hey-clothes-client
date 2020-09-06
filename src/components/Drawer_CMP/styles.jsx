a1ac84ea22f9e75f964d0548566eafa3b8406581
Gabriel Loch <grl.loch@gmail.com>
2020-09-06 20:31:18 -0300

    Drawer style updated

--- src/components/Drawer_CMP/styles.jsx
+++ src/components/Drawer_CMP/styles.jsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { fontSize } from "../../styles/typography";
 
 export const Overlay = styled.div`
   position: fixed;
@@ -15,14 +16,31 @@ export const Overlay = styled.div`
 `;
 
 export const DrawerBody = styled.div`
-    position: fixed;
-    right: 0;
-    top: 0;
-    z-index: 1100;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 1100;
 
-    overflow-y: auto;
-    width: 80%;
-    height:100%;
-    background: #c4c4c4;
-    padding: 2rem;
-`
+  overflow-y: auto;
+  width: 80%;
+  height: 100%;
+  background: #c4c4c4;
+  padding: 2rem;
+
+  form {
+    label {
+      font-size: ${fontSize.h6}em;
+    }
+    input {
+      font-size: ${fontSize.h5}em;
+      padding: 0.5rem 1rem;
+    }
+    button {
+      display: block;
+      padding: 1.5rem 2rem;
+      margin: 3rem auto;
+      width:100%;
+      max-width: 300px;
+    }
+  }
+`;
