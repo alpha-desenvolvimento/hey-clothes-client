62d7ca4bced1b60c3d6e1535d5a8b7507efaf091
Gabriel Loch <grl.loch@gmail.com>
2020-09-06 21:23:12 -0300

    Auth updated

--- src/API/HerokuServer/auth.js
+++ src/API/HerokuServer/auth.js
@@ -1,24 +1,33 @@
 import React, { useContext } from "react";
 import { Redirect } from "react-router-dom";
 
+import { users } from "./database";
+
 export default class Auth {
   static async user(args = { user: null, pwd: null }) {
     const { user, pwd } = args;
 
-    if (user == "e@mail.com" && pwd == "123456") {
-      const section = {
-        name: "user teste",
-        token: "igoréumgato",
-        status: "success",
-        error: null,
-      };
+    for (const u of users)
+      if (user == u.email && pwd == u.password) {
+        const section = {
+          name: u.name,
+          token: "MockToKeN",
+          status: "success",
+          error: null,
+        };
 
-      window.localStorage.setItem("section", JSON.stringify(section));
+        window.localStorage.setItem("section", JSON.stringify(section));
 
-      return section;
-    } else {
-      return { name: null, token: null, status: "fail", error: "custom error" };
-    }
+        return section;
+      }
+
+    return {
+      name: null,
+      token: null,
+      status: "fail",
+      error:
+        "Erro de autenticação: usuário não existe ou informações estão incorretas",
+    };
   }
 
   static async token(token) {
