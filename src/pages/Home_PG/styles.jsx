4e3404669babe9dc33162bbb60f2c16dc8576c5b
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-17 18:22:42 -0300

    updated Home styling

--- src/pages/Home_PG/styles.jsx
+++ src/pages/Home_PG/styles.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import colors from "../../styles/colors";
+import { mediaQuery } from "../../styles/global";
 
 const mainBgColor = `rgba(${colors.primary.rgb.r},${colors.primary.rgb.g},${colors.primary.rgb.b},0.3)`;
 // const formBgColor = `rgba(${colors.secondary.rgb.r},${colors.secondary.rgb.g},${colors.secondary.rgb.b}, 1)`;
@@ -10,38 +11,27 @@ export const Img = styled.img`
   width: 90%;
   max-width: 250px;
 `;
+
 export const Main = styled.main`
-  background: ${mainBgColor};
+  background: #f2f2f2;
   width: 100vw;
   height: 100vh;
-  padding: 3rem;
+  padding: 2.4rem;
 
   .form-wrapper {
-    background-color: ${colors.secondary.hex};
+    /*background-color: ${colors.secondary.hex};*/
+    background-color: #625656;
     color: ${colors.white.hex};
-    
   }
+
   h3 {
     text-align: right;
   }
-  label{
+  label {
     color: ${colors.white.hex};
   }
-  input {
-    color: ${colors.white.hex};
-    border-bottom: 2px solid ${colors.white.hex};
-    text-align: center;
 
-    &:focus {
-      border-bottom: 2px solid ${colors.tertiary.hex};
-    }
-    &::placeholder {
-      color: ${colors.white.hex}99;
-    }
-  }
-  button{
-    background-color: ${colors.tertiary.hex};
-    border:0;
-    color: ${colors.white.hex};
+  ${mediaQuery[1]} {
+    padding: 4.8rem;
   }
 `;
