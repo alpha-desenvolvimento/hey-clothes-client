7fe859402538dbb0931e6a0cec8e12aa5d5a81bf
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-17 18:21:53 -0300

    updated LoginForm

--- src/components/LoginForm_CMP/index.jsx
+++ src/components/LoginForm_CMP/index.jsx
@@ -1,43 +1,35 @@
-import React, { useCallback, useContext, useState } from "react";
+import React, { useContext } from "react";
 import { withRouter, Redirect } from "react-router";
+import { useForm } from "react-hook-form";
 
 import { AuthContext } from "../../AuthContext";
 import Button from "../Button_CMP";
-import { FormWrapper, Input, Label, Wrapper, Error } from "./styles";
+import { FormWrapper, Input, Label, Wrapper, ForgotPwdText } from "./styles";
 
 // import AuthCTR from "../../controller/auth_CTR";
 import HerokuServer from "../../API/HerokuServer";
 
 const Login = ({ history, className }) => {
+  const { register, handleSubmit, errors } = useForm({
+    mode: "onChange",
+  });
+
   const { currentUser, setCurrentUser } = useContext(AuthContext);
-  const [userValues, setValues] = useState({ pwd: undefined, user: undefined });
 
-  const handleLogin = useCallback(
-    async (event) => {
-      event.preventDefault();
-      window.localStorage.setItem("section", null);
-      const { user, pwd } = userValues;
-      setCurrentUser(null);
-      if (!user) {
-      }
-      if (!pwd) {
-      } else {
-        await HerokuServer.Auth.user({ user, pwd }).then((resp) => {
-          if (resp.status === "success") setCurrentUser({ ...resp });
-        });
-      }
-    },
-    [setCurrentUser, userValues]
-  );
-
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-
-    let nValue = userValues;
-    nValue[name] = value;
-
-    setValues(nValue);
-  }
+  const handleLogin = async (formValues) => {
+    console.log(formValues);
+    window.localStorage.setItem("section", null);
+    const { user, pwd } = formValues;
+    setCurrentUser(null);
+    if (!user) {
+    }
+    if (!pwd) {
+    } else {
+      await HerokuServer.Auth.user({ user, pwd }).then((resp) => {
+        if (resp.status === "success") setCurrentUser({ ...resp });
+      });
+    }
+  };
 
   if (currentUser) return <Redirect to="/p" />;
 
@@ -45,31 +37,45 @@ const Login = ({ history, className }) => {
     <Wrapper>
       <FormWrapper className={className}>
         <h3>Log in</h3>
-        <form onSubmit={handleLogin}>
-          <Label> Email </Label>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <Label htmlFor="user"> Email </Label>
           <Input
+            id="inputUser"
+            ref={register({
+              required: "please, enter an email adress.",
+              pattern: /^\S+@\S+$/i,
+            })}
             name="user"
             type="email"
-            value={userValues.user}
+            error={errors.user}
+            //value={userValues.user}
             placeholder="meu@email.com"
-            onInput={handleInputChange}
+            //onInput={handleInputChange}
           />
-          <Label> Password </Label>
+
+          <Label htmlFor="pwd"> Password </Label>
           <Input
+            id="inputPwd"
+            ref={register({ required: true })}
             name="pwd"
             type="password"
-            value={userValues.pwd}
+            error={errors.pwd}
+            //value={userValues.pwd}
             placeholder="Password"
-            onInput={handleInputChange}
+            //onInput={handleInputChange}
           />
+
           <Button
             primary
             type="submit"
             style={{ margin: "5rem auto 2rem", display: "block" }}
           >
-            Entrar
+            Login
           </Button>
         </form>
+        <ForgotPwdText href="#a">
+          <span>Forgot your password?</span>
+        </ForgotPwdText>
       </FormWrapper>
     </Wrapper>
   );
