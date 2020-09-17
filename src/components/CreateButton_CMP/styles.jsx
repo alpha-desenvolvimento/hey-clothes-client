46b1c4756296221dc265f7cd3611ae14f7aa68ab
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-17 18:30:19 -0300

    updated CreateButton_CMP styling

--- src/components/CreateButton_CMP/styles.jsx
+++ src/components/CreateButton_CMP/styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mediaQuery } from "../../styles/global";
 
 const btnSize = "5rem";
 export const Button = styled.button`
@@ -17,8 +18,31 @@ export const Button = styled.button`
   border: 0;
 
   color: #ffffff;
-  background-color: #a2cc8c;
-  box-shadow: 0 2px 6px 0 hsla(0, 0%, 0%, 0.2);
+  background-color: #3cbc39;
 
   cursor: pointer;
+
+  transition: all 0.2s ease-in;
+  will-change: opacity box-shadow;
+
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+
+  &:hover,
+  &:focus {
+    opacity: 0.9;
+    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+      0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  }
+
+  &:active {
+    opacity: 1;
+    box-shadow: 0px 5px 5px -3px rgba(0, 0, 0, 0.2),
+      0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12);
+  }
+
+  ${mediaQuery[0]} {
+    bottom: 4.8rem;
+    right: 4.8rem;
+  }
 `;
