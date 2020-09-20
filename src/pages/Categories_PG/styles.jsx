248f967fa7f213adccf3ba746b39ea2d1e76b88d
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-19 23:24:21 -0300

    updated Categories_PG styling

--- src/pages/Categories_PG/styles.jsx
+++ src/pages/Categories_PG/styles.jsx
@@ -1,43 +1,8 @@
 import styled from "styled-components";
 import { mediaQuery } from "../../styles/global";
 
-export const Table = styled.table`
-  display: block;
-  padding: 5rem 1.6rem;
-  tr {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-
-    th,
-    td {
-      text-align: left;
-      width: 100%;
-      margin-bottom: 2rem;
-      width: 49%;
-    }
-
-    td {
-      position: relative;
-
-      border-bottom: 1px solid rgb(0 0 0 / 0.3);
-      padding: 0.5rem 1rem 0.3rem;
-
-      .icon {
-        position: absolute;
-        right: 0.3rem;
-        top: 1rem;
-        cursor: pointer;
-      }
-    }
-  }
-  tr th {
-    padding-left: 3rem;
-  }
-`;
-
 export const CardContainer = styled.section`
-  padding: 5rem 1.6rem;
+  padding: 12rem 1.6rem 5rem;
 
   display: flex;
   flex-flow: wrap column;
