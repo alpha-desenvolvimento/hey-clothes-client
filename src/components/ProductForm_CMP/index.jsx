79f18d8342194628f330443e0c0cf4c99747a5d4
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-21 03:36:45 -0300

    fixed ProductForm fetch

--- src/components/ProductForm_CMP/index.jsx
+++ src/components/ProductForm_CMP/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 import {
   Form,
@@ -11,73 +12,103 @@ import {
   IdText,
 } from "./styles";
 
-import HerokuServer from "../../API/HerokuServer";
-
-// import { Produtos as produtos } from "../../pages/Products_PG/dbProd";
-
-const ProductForm = ({ prodId, isCreate, ...rest }) => {
+const ProductForm = ({ prodId, isCreate, refreshData }) => {
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
   const [currentProduct, setCurrentProduct] = useState(null);
 
+  const fetchAndSetData = () => {
+    let url = `${process.env.REACT_APP_API_URL}/api/products/${prodId}`;
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log("resp", resp);
+        // resp.header. TODO coloda o erro que vem no header
+        setCurrentProduct(resp.data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
-    if (isCreate) setCurrentProduct(HerokuServer.Product.mockProd);
-    else
-      HerokuServer.Product.get(prodId).then((resp) => setCurrentProduct(resp));
-  });
+    if (isCreate) {
+      setCurrentProduct({
+        name: "",
+        phone: "",
+      });
+    } else {
+      fetchAndSetData();
+    }
+  }, []);
+
+  const onSubmit = (formData) => {
+    //TODO implementar onSubmit direito
+    const action = isCreate ? "create" : "update";
+    const url = `${process.env.REACT_APP_API_URL}/api/products/${action}`;
+
+    const id = !isCreate && currentProduct.id;
+    const name = formData.providerName;
+    const phone = formData.providerPhone;
+
+    axios.post(url, { id, name, phone }).then((resp) => {
+      console.log("updatedProvider resp", resp);
+      refreshData();
+      fetchAndSetData();
+    });
+  };
 
   function sucessLoad() {
     return (
-      <>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <h4>{currentProduct.name}</h4>
-          {currentProduct.id && <IdText>ID: {currentProduct.id}</IdText>}
-          <br />
-          <br />
-          <br />
-          <Label htmlFor="productName">Nome do Produto</Label>
-          <Input
-            name="productName"
-            defaultValue={currentProduct.name}
-            ref={register({ required: true })}
-          />
-          {errors.productName && <ErrorText>Este campo é necessário</ErrorText>}
-          <Label htmlFor="productPrice">Preço do Produto</Label>
-          <Input
-            name="productPrice"
-            defaultValue={currentProduct.price}
-            ref={register({ required: true, min: 0.01 })}
-          />
-
-          <Label htmlFor="productBrand">Marca</Label>
-          <Input
-            name="productBrand"
-            defaultValue={currentProduct.brand}
-            ref={register({ required: true })}
-          />
-
-          <Label htmlFor="productCategory">Categoria</Label>
-          <Input
-            name="productCategory"
-            defaultValue={currentProduct.category}
-            ref={register({ required: true })}
-          />
-
-          {errors.productPrice && (
-            <ErrorText>Este campo é necessário</ErrorText>
-          )}
-
-          <Label htmlFor="productPhoto">Fotos do Produto</Label>
-          <PhotoContainer>
-            {currentProduct.photo && (
-              <ProductPhoto src={currentProduct.photo} />
+      console.log(currentProduct), //TODO atualizar os campos do form
+      (
+        <>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <h4>{currentProduct.name}</h4>
+            {currentProduct.id && <IdText>ID: {currentProduct.id}</IdText>}
+            <br />
+            <br />
+            <br />
+            <Label htmlFor="productName">Nome do Produto</Label>
+            <Input
+              name="productName"
+              defaultValue={currentProduct.name}
+              ref={register({ required: true })}
+            />
+            {errors.productName && (
+              <ErrorText>Este campo é necessário</ErrorText>
             )}
-            {currentProduct.pictures.length > 0 &&
-              currentProduct.pictures.map((picture, index) => (
-                <ProductPhoto key={index} src={picture} />
-              ))}
-          </PhotoContainer>
-          {/* <Input
+            <Label htmlFor="productPrice">Preço do Produto</Label>
+            <Input
+              name="productPrice"
+              defaultValue={currentProduct.price}
+              ref={register({ required: true, min: 0.01 })}
+            />
+
+            <Label htmlFor="productBrand">Marca</Label>
+            <Input
+              name="productBrand"
+              defaultValue={currentProduct.brand}
+              ref={register({ required: true })}
+            />
+
+            <Label htmlFor="productCategory">Categoria</Label>
+            <Input
+              name="productCategory"
+              defaultValue={currentProduct.category}
+              ref={register({ required: true })}
+            />
+
+            {errors.productPrice && (
+              <ErrorText>Este campo é necessário</ErrorText>
+            )}
+
+            <Label htmlFor="productPhoto">Fotos do Produto</Label>
+            <PhotoContainer>
+              {currentProduct.imgA && (
+                <ProductPhoto src={currentProduct.photo} />
+              )}
+            </PhotoContainer>
+            {/* <Input
             name="productPhoto"
             defaultValue={currentProduct.photo}
             ref={register({ required: true })}
@@ -85,9 +116,10 @@ const ProductForm = ({ prodId, isCreate, ...rest }) => {
           {errors.productPhoto && (
             <ErrorText>Este campo é necessário</ErrorText>
           )} */}
-          <button type="submit">Salvar</button>
-        </Form>
-      </>
+            <button type="submit">Salvar</button>
+          </Form>
+        </>
+      )
     );
   }
 
