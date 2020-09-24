b28be3c5caf888243c457c099657262f82cd3ebb
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-24 01:14:36 -0300

    added CssBaseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-
 import { AuthProvider } from "./AuthContext";
 import Routes from "./routes";
 import GlobalStyles from "./styles/global";
 import Typography from "./styles/typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
   return (
     <AuthProvider>
       <GlobalStyles />
       <Typography />
+      <CssBaseline />
       <Routes />
     </AuthProvider>
   );
