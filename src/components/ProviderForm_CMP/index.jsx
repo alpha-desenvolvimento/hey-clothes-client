0299f31821d2f2ae56729794cf2328c6f1b8b2c6
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-25 03:52:29 -0300

    updated ProviderForm

--- src/components/ProviderForm_CMP/index.jsx
+++ src/components/ProviderForm_CMP/index.jsx
@@ -2,11 +2,28 @@ import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 
-import { Form, Input, Label } from "./styles";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
-const CategoryForm = ({ providerId, isCreate, refreshData }) => {
+import Loading from "../MaterialLoading_CMP";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: "1rem",
+      width: "100%",
+    },
+  },
+}));
+
+const ProviderForm = ({ providerId, isCreate, refreshData }) => {
   const { register, handleSubmit, errors } = useForm();
   const [currentProvider, setCurrentProvider] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const classes = useStyles();
 
   const fetchAndSetData = () => {
     let url = `${process.env.REACT_APP_API_URL}/api/provider/${providerId}`;
@@ -19,6 +36,9 @@ const CategoryForm = ({ providerId, isCreate, refreshData }) => {
       })
       .catch((err) => {
         console.log(err);
+      })
+      .finally(() => {
+        setIsLoaded(true);
       });
   };
 
@@ -44,8 +64,10 @@ const CategoryForm = ({ providerId, isCreate, refreshData }) => {
     const id = !isCreate && currentProvider.id;
     const name = formData.providerName;
     const phone = formData.providerPhone;
+    const email = formData.providerEmail;
+    const endereco = formData.providerAdress;
 
-    axios.post(url, { id, name, phone }).then((resp) => {
+    axios.post(url, { id, name, phone, email, endereco }).then((resp) => {
       console.log("updatedProvider resp", resp);
       refreshData();
       fetchAndSetData();
@@ -54,29 +76,56 @@ const CategoryForm = ({ providerId, isCreate, refreshData }) => {
 
   function sucessLoad() {
     return (
-      <>
-        <Form onSubmit={handleSubmit(onSubmit)}>
+      <Box padding="2rem" fontSize="2.4rem">
+        <form
+          className={classes.root}
+          autoComplete="off"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <h4>{isCreate ? "Novo fornecedor" : currentProvider.name}</h4>
 
-          <Label htmlFor="providerName">Nome</Label>
-          <Input
+          <TextField
+            variant="outlined"
+            label="Nome"
             type="text"
             defaultValue={currentProvider.name}
             name="providerName"
             ref={register({ required: true, maxLength: 80 })}
           />
 
-          <Label htmlFor="providerPhone">Telefone</Label>
-          <Input
+          <TextField
+            variant="outlined"
+            label="Telefone"
             type="text"
             defaultValue={currentProvider.phone}
             name="providerPhone"
             ref={register({ required: true, maxLength: 15 })}
           />
 
-          <button type="submit">Salvar</button>
-        </Form>
-      </>
+          <TextField
+            variant="outlined"
+            label="Email"
+            type="email"
+            defaultValue={currentProvider.email}
+            name="providerEmail"
+            ref={register()}
+          />
+
+          <TextField
+            variant="outlined"
+            multiline
+            label="Endereço"
+            type="text"
+            defaultValue={currentProvider.endereco}
+            name="providerAdress"
+            ref={register()}
+          />
+
+          <Button color="primary" variant="contained" type="submit">
+            Salvar
+          </Button>
+        </form>
+      </Box>
     );
   }
 
@@ -84,7 +133,17 @@ const CategoryForm = ({ providerId, isCreate, refreshData }) => {
     return <h4>Erro: Fornecedor não localizado</h4>;
   }
 
-  return <>{currentProvider ? sucessLoad() : errorLoad()}</>;
+  return (
+    <>
+      {!isLoaded && !isCreate ? (
+        <Loading />
+      ) : currentProvider ? (
+        sucessLoad()
+      ) : (
+        errorLoad()
+      )}
+    </>
+  );
 };
 
-export default CategoryForm;
+export default ProviderForm;
