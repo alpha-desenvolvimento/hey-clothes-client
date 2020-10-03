0d303566af9cf2f4fc70e043babfe39b727e0b03
Isaac Ziroldo <isaac.ziroldo@gmail.com>
2020-10-03 15:19:38 -0300

    CategoryTable
    Return products related

--- src/components/Table_CMP/index.jsx
+++ src/components/Table_CMP/index.jsx
@@ -10,30 +10,24 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
 import axios from "axios";
-import { getUrlParams } from "../../controller/url";
-import { useParams, useHistory } from "react-router-dom";
 
+
+let prods = []
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+    minWidth: 250,
   },
 });
 
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
+function createData(name, price) {
+  return { name, price};
+}
 
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
+
 
 const BasicTable = ({categoryId,isCreate}) => {
   const classes = useStyles();
-  const [currentCategory, setCurrentCategory] = useState(null);
+  const [currentCategory, setCurrentCategory] = useState(true);
   const [currentProducts, setCurrentProducts] = useState(true);
   const [isLoadingCategory, setIsLoadingCategory] = useState(true);
   const [checked, setChecked] = useState(true);
@@ -41,16 +35,14 @@ const BasicTable = ({categoryId,isCreate}) => {
 
   const fetchAndSetData = () => {
     setIsLoadingCategory(true);
-
     let url = `${process.env.REACT_APP_API_URL}/api/category/${categoryId}`;
     axios
       .get(url)
       .then((resp) => {
-        console.log("resp", resp);
+        console.log(resp.data.products)
         // resp.header. TODO coloda o erro que vem no header
-        setCurrentCategory(resp.data);
-        setCurrentProducts(currentCategory.products);
-        console.log(currentCategory)
+        setCurrentCategory(resp.data)
+        prods = resp.data.products
         setChecked(resp.data.isActive == 1);
         // setAllowDelete(!resp.data.hasProduct);
       })
@@ -62,7 +54,7 @@ const BasicTable = ({categoryId,isCreate}) => {
       });
       
     }
-      useEffect(() => {
+    useEffect(() => {
         if (isCreate) {
           setCurrentProducts({
             name: "",
@@ -73,9 +65,7 @@ const BasicTable = ({categoryId,isCreate}) => {
             fetchAndSetData();
         }
       }, []);
-      
-      let products = Array.from(currentProducts)
-      console.log(products)
+
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
@@ -87,18 +77,21 @@ const BasicTable = ({categoryId,isCreate}) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {products.map(product => (
+            {prods && (
+                <>
+          {prods.map(product => (
             <TableRow key={product.name}>
               <TableCell component="th" scope="row">
                 {product.name}
               </TableCell>
-              <TableCell align="right">{product.name}</TableCell>
-              <TableCell >{product.price}</TableCell>
+              <TableCell align="right">{product.price}</TableCell>
+              {/* <TableCell align="right">{product.price}</TableCell> */}
               
             </TableRow>
           ))
-          
-          }
+            }
+          </>
+          )}
         </TableBody>
       </Table>
     </TableContainer>
@@ -106,5 +99,7 @@ const BasicTable = ({categoryId,isCreate}) => {
 
 
 }
+
+
 
 export default BasicTable;
